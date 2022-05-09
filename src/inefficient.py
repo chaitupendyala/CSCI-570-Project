e1ccee51b3b08a4fa2255f48a35be5c4677e103b
chaitupendyala <prndyala@gmail.com>
2022-05-08 19:52:03 -0700

    Changing logic of checking memory consumption.

--- src/inefficient.py
+++ src/inefficient.py
@@ -94,12 +94,12 @@ def Find_Alignment(Alignment_Costs, A, B):
 def call_inefficient_sequence_alignment( s1, s2, output_file ):
     start = time.time()
     process = psutil.Process()
-    memory_info = process.memory_info()
 
     Alignment_Costs = Alignment(s1, s2)
     wf = Find_Alignment(Alignment_Costs, s1, s2)
     end = time.time()
     run_time = end - start
+    memory_info = process.memory_info()
     memory_consumed = int(memory_info.rss/1024)
 
     wf.append(str(run_time*1000))
